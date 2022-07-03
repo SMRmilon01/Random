a243095d639ae587c9ffbc5daa7edc972a33b495
Smr milon <108234691+SMRmilon01@users.noreply.github.com>
2022-07-03 13:01:38 +0600

    Add files via upload

---
+++ AKING.py
@@ -0,0 +1,20 @@
+import os, platform, time
+try:
+    import requests
+except:
+    os.system('pip install requests')
+    os.system('git pull')
+    import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    print("\n\x1b[1;92m Congratulations ! Your Device Support this Tools")
+    print(' Join Over Facebook Group For Any Help 😍 ')
+    os.system('xdg-open https://www.facebook.com/groups/351076900316263/?ref=share');time.sleep(3)
+    from AKING64 import main
+    main()
+elif bit == '32bit':
+    print("\x1b[1;92m Congratulations ! Your Device Support this Tools")
+    print(' Join Over Facebook Group For Any Help 😍 ')
+    os.system('xdg-open https://www.facebook.com/groups/351076900316263/?ref=share');time.sleep(3)
+    from AKING32 import main
+    main()
